81ddac4cbaa41fe1518edd68c45b7a8d7eb9137e
Guillaume Rémy-Zéphir <gui.remyzephir@gmail.com>
2016-05-26 14:20:24 +0200

    Button
    ressource.css

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
     <title>PFA - Fondation de France - Equipe 9</title>
     <link rel="stylesheet" href="style/normalize.css">
     <link rel="stylesheet" href="style/header.css">
+    <link rel="stylesheet" href="style/ressources.css">
 </head>
 
 <body>
 
     <div class="main-navbar">
         <a href="#"><img class="logo-navbar" src="#" alt="Logo"></a>
-        <a href="#">Soumettre mon projet</a>
-        <a href="#">Accéder aux contenu exclusif et financer des projets gratuitement !</a>
+        <button class="button button-secondary">Soumettre mon projet</button>
+        <button class="button button-primary">Connexion</button>
     </div>
-
 
     <p>Projet #1</p>
     <p>Projet #2</p>

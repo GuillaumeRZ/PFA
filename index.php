3f1f4ed8fdc12602946a1b03bf17d15ee1eeebbf
Guillaume Rémy-Zéphir <gui.remyzephir@gmail.com>
2016-05-26 11:47:51 +0200

    Starting the header
    normalize + header.css

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>PFA - Fondation de France - Equipe 9</title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/header.css">
 </head>
+
 <body>
-    <p>test</p>
+
+    <div class="main-navbar">
+        <a href="#"><img class="logo-navbar" src="#" alt="Logo"></a>
+        <a href="#">Soumettre mon projet</a>
+        <a href="#">Accéder aux contenu exclusif et financer des projets gratuitement !</a>
+    </div>
+
+
+    <p>Projet #1</p>
+    <p>Projet #2</p>
+    <p>Projet #3</p>
+    <p>Projet #4</p>
+    <p>Projet #5</p>
+
+    <p>Footer</p>
 </body>
+
 </html>
